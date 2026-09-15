94d9e73e1f215ea8a279c30b1358a1bd68f41557
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2026-09-15 12:46:58 +0200

    revert: export images without `schema:url`

---
+++ .changeset/old-teeth-exist.md
@@ -0,0 +1,5 @@
+---
+"piveau-opendata-swiss": patch
+---
+
+Fix the missing showcase images not imported from the CMS
--- opendata.swiss/ui/server/api/showcases.ts
+++ opendata.swiss/ui/server/api/showcases.ts
@@ -16,7 +16,7 @@ interface AggregateShowcase {
   '@type': string[]
   'type': string
   'title': Record<string, string | undefined>
-  'images': Array<{ url: string }>
+  'images': string[]
   'abstract': Record<string, string | undefined>
   'themes': string[]
   'datasets': Array<{ identifier: string, label: string }>
@@ -126,7 +126,7 @@ export default defineEventHandler(async (event) => {
         '@type': ['Showcase', 'Dataset', 'piveau:CustomResource'],
         'type': showcase.type,
         'title': {},
-        'images': showcase.images.map(image => ({ url: image.image })) || [],
+        'images': showcase.images.map(image => image.image) || [],
         'abstract': {},
         'themes': showcase.themes || [],
         'datasets': mapDatasets(showcase.datasets) || [],
